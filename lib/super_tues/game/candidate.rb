3da3da0afe6370b5d483c3c06accf935628e765f
John House <jonlhouse@gmail.com>
2014-01-21 17:48:51 -0600

    added candidate class

--- lib/super_tues/game/candidate.rb
+++ lib/super_tues/game/candidate.rb
@@ -2,7 +2,13 @@ module SuperTues
   module Game
 
     class Candidate
-      
+
+      attr_reader :name, :clout, :cash, :cards, :description
+
+      def initialize(attrs)
+        attrs.each { |attr,value| instance_variable_set("@#{attr}", value) }
+      end
+
     end
 
   end
