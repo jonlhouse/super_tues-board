831cd32f40bb9f0f6340ef15d8017eb82ddf3cd9
John House <jonlhouse@gmail.com>
2014-01-28 15:32:01 -0600

    began actions/play_cards specs

--- lib/super_tues/game/actions/play_card.rb
+++ lib/super_tues/game/actions/play_card.rb
@@ -3,8 +3,26 @@ module SuperTues
     module Actions
 
       class PlayCard < Action
-        def initialize(attrs = {})
+
+        # Creates a new play_card action.
+        #
+        # Usage:
+        #   PlayCard.new(card)
+        #
+        def initialize(card)
           super
+          @card = card
+        end
+
+        def allowed?(rules)
+          tests = [:can_play_cards?]
+          tests.all? { |test| pass? test, rules }
+        end
+
+      private
+
+        def can_play_cards?(rules)
+          rules['player.can_play_card', true, player: :current]
         end
       end
 
--- spec/super_tues/game/actions/play_card_spec.rb
+++ spec/super_tues/game/actions/play_card_spec.rb
@@ -5,6 +5,19 @@ module SuperTues
     module Actions
       describe PlayCard do
         specify { PlayCard.should < Action }
+
+        let(:card) { double(title: '', description: '', effect: {}) }
+
+        describe "initialize" do
+          specify { PlayCard.new(card).instance_variable_get(:@card).should == card }
+        end
+
+        describe "#allowed?" do
+          let(:rules) { Board.new.rules }
+          context "true when" do
+            specify { PlayCard.new(card).allowed?(rules).should be }
+          end
+        end
       end
     end
   end
