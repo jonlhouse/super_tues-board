7be543b0bdc97e81c70b6d05be3d4410c04c57c7
John House <jonlhouse@gmail.com>
2014-01-28 17:21:10 -0600

    basic discard allowed rules passing

--- lib/super_tues/game/actions/discard.rb
+++ lib/super_tues/game/actions/discard.rb
@@ -3,9 +3,36 @@ module SuperTues
     module Actions
 
       class Discard < Action
-        def initialize(card_or_cards)
-          super
-          # @cards = Array.wrap(card_or_cards)
+
+        # Creates a new discard action.
+        #
+        # Usage:
+        #   Discard.new(card)
+        #   Discard.new(card1, card2)
+        #   Discard.new([card1, card2])
+        #
+        def initialize(*cards, **opts)
+          super opts
+          @cards = Array.wrap(cards).flatten
+        end
+
+        def allowed?(rules)
+          tests = [:can_discard?, :lte_max_cards?]
+          tests.all? { |test| pass? test, rules }
+        end
+
+        def count
+          @cards.length
+        end
+
+      private
+
+        def can_discard?(rules)
+          rules.player('action.discard.allowed')
+        end
+
+        def lte_max_cards?(rules)
+          count <= rules.player('action.discard.max')
         end
       end
 
