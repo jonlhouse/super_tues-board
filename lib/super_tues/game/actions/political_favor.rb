a0ecfe436eb82bffab0aec662ef3b0b5293e9807
John House <jonlhouse@gmail.com>
2014-01-28 00:41:12 -0600

    political favor specs in

--- lib/super_tues/game/actions/political_favor.rb
+++ lib/super_tues/game/actions/political_favor.rb
@@ -4,8 +4,25 @@ module SuperTues
 
       class PoliticalFavor < Action
         def initialize(attrs = {})
-          super
+          super(attrs)
+          @state_picks = attrs
         end
+
+        def allowed?(rules)
+          tests = [:can_play_picks?, :more_than_zero_picks?, :less_than_max_picks?, :spread_allowed?]
+          tests.all? { |test| pass? test, rules}
+        end
+
+      private
+
+        def more_than_zero_picks?(rules)
+          total_picks > 0
+        end
+
+        def less_than_max_picks?(rules)
+          total_picks <= rules['action.radio_spot.picks.max']
+        end
+
       end
 
     end
--- spec/super_tues/game/actions/political_favor_spec.rb
+++ spec/super_tues/game/actions/political_favor_spec.rb
@@ -3,9 +3,35 @@ require 'spec_helper'
 module SuperTues
   module Game
     module Actions
+
       describe PoliticalFavor do
         specify { PoliticalFavor.should < Action }
-      end
+
+        describe "#initialize(params)" do
+          attrs = { 'Iowa' => 5 }
+          favor = PoliticalFavor.new attrs
+          favor.instance_variable_get(:@state_picks).should == attrs
+        end
+
+        describe "#allowed?" do
+          let(:rules) { Board.new.rules }
+          context "true when" do
+            it "total picks <= max" do
+              PoliticalFavor.new('Iowa' => 5).allowed?(rules).should be
+            end
+          end
+          context "false when" do
+            it "total picks < 1" do
+              PoliticalFavor.new('Iowa' => 0).allowed?(rules).should_not be
+            end
+            it "total picks > max" do
+              PoliticalFavor.new('Iowa' => 6).allowed?(rules).should_not be
+            end
+          end         
+        end
+
+      end      
     end
+
   end
 end
