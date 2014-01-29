f6871a07a5904e04b03f5b9a4ad5493ea9dc85d2
John House <jonlhouse@gmail.com>
2014-01-29 14:51:06 -0600

    refactored player specs

--- spec/super_tues/game/sim1_spec.rb
+++ spec/super_tues/game/sim1_spec.rb
@@ -5,11 +5,25 @@ module SuperTues
 
     describe "SuperTues game simulation (1)" do
 
-      let(:player1) { Player.new(name: 'player-1', color: :red) }
-      let(:player2) { Player.new(name: 'player-2', color: :blue) }
-      let(:player3) { Player.new(name: 'player-3', color: :green) }
-      let(:player4) { Player.new(name: 'player-4', color: :yellow) }
+      let(:player0) { Player.new(name: 'player-0', color: :red) }
+      let(:player1) { Player.new(name: 'player-1', color: :blue) }
+      let(:player2) { Player.new(name: 'player-2', color: :green) }
+      let(:player3) { Player.new(name: 'player-3', color: :yellow) }
 
+      let(:board) { Board.new }
+
+      describe "during game setup" do
+        describe "assigns players to the board" do
+          it "seats players" do
+            board.seat player0
+            board.seat player1
+            board.seat player2
+            board.seat player3
+            board.seats.should == [player0, player1, player2, player3]
+          end
+          
+        end
+      end
     end
 
   end
