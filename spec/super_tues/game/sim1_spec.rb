286685fcfa048244671f552adcf73aaa0dab423c
John House <jonlhouse@gmail.com>
2014-01-29 14:29:11 -0600

    added game sim spec

---
+++ spec/super_tues/game/sim1_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+module SuperTues
+  module Game
+
+    describe "SuperTues game simulation (1)" do
+
+      let(:player1) { Player.new(name: 'player-1', color: :red) }
+      let(:player2) { Player.new(name: 'player-2', color: :blue) }
+      let(:player3) { Player.new(name: 'player-3', color: :green) }
+      let(:player4) { Player.new(name: 'player-4', color: :yellow) }
+
+    end
+
+  end
+end
