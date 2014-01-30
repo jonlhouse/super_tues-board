28a5117e129a5f41a7cbc5774519138b8fe121aa
John House <jonlhouse@gmail.com>
2014-01-30 01:08:11 -0600

    added showing state to card

--- lib/super_tues/game/card.rb
+++ lib/super_tues/game/card.rb
@@ -6,9 +6,22 @@ module SuperTues
       attr_reader :title, :description, :effect
 
       def initialize(attrs)
+        @showing = false
         @title = attrs[:title]
         @description = attrs[:description]
         @effect = attrs[:effect]
+      end
+
+      def up?
+        @showing
+      end
+
+      def down?
+        not up?
+      end
+
+      def show
+        @showing = true
       end
 
     end
--- spec/super_tues/game/card_spec.rb
+++ spec/super_tues/game/card_spec.rb
@@ -20,6 +20,10 @@ module SuperTues
       specify { card.title.should == 'Visit Sunny Florida' }
       specify { card.description.should == 'Play 3 picks in Texas\'s bin.' }
       specify { card.effect.should be_a Hash }
+      specify { card.up?.should_not be }
+      specify { card.down?.should be }
+      specify { expect { card.show }.to change { card.up? } }
+
     end
 
   end
