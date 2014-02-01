bc59094923571a8fb1b7eb8dc7b64ebfcd846567
John House <jonlhouse@gmail.com>
2014-02-01 17:18:48 -0600

    card deck spec change

--- spec/super_tues/board/card_deck_spec.rb
+++ spec/super_tues/board/card_deck_spec.rb
@@ -19,13 +19,13 @@ module SuperTues
       describe "default cards" do
         let(:cards) { CardDeck.new }
         it "all have a title" do
-          cards.each { |card| expect(card.title).to_not be_nil }
+          cards.each { |card| expect(card.title).to be_a String }
         end
         it "all have a description" do
-          cards.each { |card| expect(card.description).to_not be_nil }
+          cards.each { |card| expect(card.description).to be_a String }
         end
         it "all have an effect" do
-          cards.each { |card| expect(card.effect).to_not be_nil }
+          cards.each { |card| expect(card.effect).to be_a Hash }
         end
         it "none have a count" do
           cards.each { |card| expect(card.instance_variable_defined?(:@count)).to be_false }
